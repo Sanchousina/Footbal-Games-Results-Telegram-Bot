d9cc8abf2ef8b8407d9916f62eb57999621c8409
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-03-20 13:55:16 +0100

    Handle /start command

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(
+    chatId,
+    'Привіт! Я бот для інформування про результати футбольних матчів',
+  );
+});
+
 bot.on('message', (msg) => {
   console.log(msg);
   bot.sendMessage(msg.chat.id, 'Welcome!');
