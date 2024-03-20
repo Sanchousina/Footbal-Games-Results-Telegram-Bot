8e3697680c629eb34c8d39e9c016875c83d7b42a
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-03-20 14:45:33 +0100

    Get all leagues from api_sports

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 require('dotenv').config();
+const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
-function sendLeaguesButtons(chatId) {
+async function getAllLeagues() {
+  const response = await axios.get(
+    'https://v3.football.api-sports.io/leagues',
+    {
+      headers: {
+        'x-apisports-key': process.env.API_SPORTS_API_KEY,
+      },
+    },
+  );
+  const leagues = response.data.response.slice(0, 10);
+  console.log(leagues);
+}
+
+async function sendLeaguesButtons(chatId) {
+  await getAllLeagues();
   bot.sendMessage(chatId, 'Для того, щоб знайти матч, оберіть спершу лігу', {
     reply_markup: {
       inline_keyboard: [
@@ -38,6 +53,6 @@ bot.on('callback_query', (query) => {
 });
 
 bot.on('message', (msg) => {
-  console.log(msg);
+  // console.log(msg);
   bot.sendMessage(msg.chat.id, msg.text);
 });
